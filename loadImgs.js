a44d12255e0a55b95661b98d5703037ebedfa1e8
Jason W <b3cmin@gmail.com>
2014-07-19 00:07:40 -0400

    adding pages

---
+++ loadImgs.js
@@ -0,0 +1,31 @@
+            // create a dot image
+            var dotReady = false;
+            var dotImage = new Image();
+            dotImage.onload = function(){
+                    dotReady=true;
+            };
+            dotImage.src = "dot.png";
+            
+            // create a dot image
+            var dotRedReady = false;
+            var dotRedImage = new Image();
+            dotRedImage.onload = function(){
+                    dotRedReady=true;
+            };
+            dotRedImage.src = "dotRed.png";    
+            
+            
+                        // create a dot image
+            var dotWhiteReady = false;
+            var dotWhiteImage = new Image();
+            dotWhiteImage.onload = function(){
+                    dotWhiteReady=true;
+            };
+            dotWhiteImage.src = "dotWhite.png";     
+            /* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
